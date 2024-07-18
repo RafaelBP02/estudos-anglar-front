067a7ed9ca82d6a0823a73ca67e63e09392d3b42
RafaelBP02 <rafaelbertopereira@gmail.com>
2024-07-18 14:34:56 -0300

    feat: adiciona caixa responsiva no path http://localhost:4200/primeiro-componente

--- src/app/data-binding/data-binding.component.ts
+++ src/app/data-binding/data-binding.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-data-binding',
   templateUrl: './data-binding.component.html',
   styleUrl: './data-binding.component.css'
 })
-export class DataBindingComponent {
+export class DataBindingComponent implements OnInit {
+  numero: number = 0;
 
+  constructor(){
+  }
+  ngOnInit() {
+  }
+
+  onInputChange(event: any) {
+    this.numero = event.target.value;
+  }
 }
