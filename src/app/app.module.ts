ea7a83ad7931037c18bffb9290e8ef3456a81339
RafaelBP02 <rafaelbertopereira@gmail.com>
2024-07-18 17:28:18 -0300

    feat: cria componente crud

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DataBindingComponent } from './data-binding/data-binding.component';
 import { NgifNgforComponent } from './ngif-ngfor/ngif-ngfor.component';
+import { CrudComponent } from './components/crud/crud.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DataBindingComponent,
-    NgifNgforComponent
+    NgifNgforComponent,
+    CrudComponent
   ],
   imports: [
     BrowserModule,
