b8975342e673e38a42032565928ea271021e9377
RafaelBP02 <rafaelbertopereira@gmail.com>
2024-07-18 16:45:51 -0300

    chore: utiliza o ferramental ngif e ngfor

--- src/app/ngif-ngfor/ngif-ngfor.component.ts
+++ src/app/ngif-ngfor/ngif-ngfor.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrl: './ngif-ngfor.component.css'
 })
 export class NgifNgforComponent {
+  numero: number|null = 1;
+  arrNumeros = [1, 2, 3, 4, 5, 6, 7];
 
+  constructor(){
+
+  }
+
+  onInputChange(event: any) {
+    const input = event.target as HTMLInputElement;
+    this.numero = Number(input.value);
+  }
 }
