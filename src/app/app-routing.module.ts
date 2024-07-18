2c8a98e9dc469c463ad292a3d690bb6e088acc1a
RafaelBP02 <rafaelbertopereira@gmail.com>
2024-07-18 15:33:29 -0300

    feat: adiciona botoes para facilitar a navegacao entre os componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DataBindingComponent } from './data-binding/data-binding.component';
+import { NgifNgforComponent } from './ngif-ngfor/ngif-ngfor.component';
+import { AppComponent } from './app.component';
 
 const routes: Routes = [
+  {path: '', component: AppComponent},
   {path: 'primeiro-componente', component: DataBindingComponent},
+  {path: 'ng-iffor', component: NgifNgforComponent},
 ];
 
 @NgModule({
