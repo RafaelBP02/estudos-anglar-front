afc0ff2707e0b8b1c85ec0d49dfd7da021d0e8be
RafaelBP02 <rafaelbertopereira@gmail.com>
2024-07-19 17:02:31 -0300

    feat: cria componente first-steps
    esse componente ira armazenar minha exploração/ aprendizado basico pelo framework Angular

---
+++ src/app/components/first-steps/first-steps.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-first-steps',
+  templateUrl: './first-steps.component.html',
+  styleUrl: './first-steps.component.css'
+})
+export class FirstStepsComponent {
+
+}
